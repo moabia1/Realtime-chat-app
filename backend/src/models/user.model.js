c24c5bf66e676d02e892d626427b6d2192d3ac56
moabia1 <mdmoabia81@gmail.com>
2025-09-23 10:04:07 +0530

    remove env from github

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required:true
+  },
+  email: {
+    type: String,
+    required:true
+  },
+  password: {
+    type: String,
+    required
+  },
+  avatar: {
+    public_id: String,
+    url:String
+  }
+}, {
+  timestamps:true
+})
+
+export const userModel = mongoose.model("user",userSchema)
