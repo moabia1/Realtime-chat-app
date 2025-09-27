4bc74a66eaa5ab38d3834f0d247596426d8b400a
moabia1 <mdmoabia81@gmail.com>
2025-09-27 23:29:47 +0530

    now working on register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
@@ -10,13 +11,7 @@ const Register = () => {
   const registerHandler = (e) => {};
   return (
     <div className="flex items-center justify-center w-full h-screen bg-white">
-      <form onSubmit={registerHandler}>
-        <input
-          type="text"
-          className="w-30 h-8 focus:ring-blue-600 border-blue-300"
-        />
-        <button className="text-whit">Submit</button>
-      </form>
+      
     </div>
   );
 };
