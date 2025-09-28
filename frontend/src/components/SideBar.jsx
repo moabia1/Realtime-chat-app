a88b9276aa43a80809b9512dc58d491fb995db63
moabia1 <mdmoabia81@gmail.com>
2025-09-28 23:41:39 +0530

    now start working on sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../store/slice/authSlice";
 
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+  const [showOnlineOnly, setShowOnlineOnly] = useState(false)
+  const {users,selectedUser,isUsersLoading} = useSelector((state)=>state.chat)
+  const { onlineUsers } = useSelector((state) => state.auth);
+  const dispatch = useDispatch()
 
-export default SideBar
+  useEffect(() => {
+    dispatch(getUser())
+  }, [dispatch])
+  
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
