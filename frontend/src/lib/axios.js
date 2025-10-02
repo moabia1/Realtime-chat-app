0781391cfb97162d226c77cdaaac8e76370fd8e7
moabia1 <mdmoabia81@gmail.com>
2025-10-03 00:21:06 +0530

    set-up live url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000" : "/",
-  withCredentials:true
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "https://realtime-chat-app-2cae.onrender.com"
+      : "/",
+  withCredentials: true,
+});
--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,23 +1,25 @@
-import { io } from "socket.io-client"
+import { io } from "socket.io-client";
 
 let socket = null;
 
 export const connectSocket = (userId) => {
   socket = io(
-    import.meta.env.MODE === "development" ? "http://localhost:4000" : "/",
+    import.meta.env.MODE === "development"
+      ? "https://realtime-chat-app-2cae.onrender.com"
+      : "/",
     {
-      query:{userId}
+      query: { userId },
     }
   );
 
-  return socket
-}
+  return socket;
+};
 
 export const getSocket = () => socket;
 
 export const disconnectSocket = () => {
   if (socket) {
-    socket.disconnect()
+    socket.disconnect();
     socket = null;
   }
-}
+};
