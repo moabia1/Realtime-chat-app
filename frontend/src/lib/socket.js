332c15247ebba92a9c71abb3cc1cfecf8a07c269
Md Moabia <168706779+moabia1@users.noreply.github.com>
2025-10-03 02:08:34 +0530

    Update socket.js

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -3,10 +3,7 @@ import { io } from "socket.io-client";
 let socket = null;
 
 export const connectSocket = (userId) => {
-  socket = io(
-    import.meta.env.MODE === "development"
-      ? "https://realtime-chat-app-2cae.onrender.com"
-      : "/",
+  socket = io("https://realtime-chat-app-2cae.onrender.com",
     {
       query: { userId },
     }
